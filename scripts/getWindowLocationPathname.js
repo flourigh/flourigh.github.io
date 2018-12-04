6e92d87d1405ec3abee0efe6f7799a57cd9d6e2a
Roberto Monteiro <r.monteiro@vertexdigital.co>
2018-12-04 15:48:27 -0200

    fix getscript

--- scripts/getWindowLocationPathname.js
+++ scripts/getWindowLocationPathname.js
@@ -1,11 +1,7 @@
 
 /* Routers */
 function getWindowLocation () {
-    return (window.location.pathname.substring(window.location.pathname.indexOf('/') + 1)).toLowerCase() }
+    return (window.location.pathname.substring(window.location.pathname.indexOf('/') + 1)).toLowerCase(); };
 
-var imported = document.createElement('script')
-imported.src = 'routers.js'
-document.head.appendChild(imported)
-
-console.log(getWindowLocation())
+console.log(getWindowLocation());
 /* Routers */
