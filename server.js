c8596f52c8cf09cb599299987a93e2f1958733eb
Roberto Monteiro <r.monteiro@vertexdigital.co>
2018-12-04 11:14:38 -0200

    fixes

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(process.env.PORT || 4000, function(){
-    console.log('Roberto Monteiro');
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+    console.log("Listening on Port 3000");
 });
