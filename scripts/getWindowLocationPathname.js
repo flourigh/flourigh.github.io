f1e3f8cc66e69493cc75b0da351a2837844ac859
Roberto Monteiro <r.monteiro@vertexdigital.co>
2018-12-04 15:39:43 -0200

    fix getscript

--- scripts/getWindowLocationPathname.js
+++ scripts/getWindowLocationPathname.js
@@ -3,8 +3,10 @@
 function getWindowLocation () {
     return (window.location.pathname.substring(window.location.pathname.indexOf('/') + 1)).toLowerCase() }
 
-$.getScript("routers.js", function () {
-   console.log("Script loaded but not necessarily executed.")
+$(document).ready(function() {
+    $.getScript("routers.js", function () {
+       console.log("Script loaded but not necessarily executed.")
+    })
 })
 
 console.log(getWindowLocation())
