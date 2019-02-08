523189618587b5ba4c454783d0a0f2caa298d6d7
Roberto Monteiro <r.monteiro@vertexdigital.co>
2019-02-08 11:49:10 -0200

    service worker

--- sw.js
+++ sw.js
@@ -1,29 +1,29 @@
 self.addEventListener('install', function(event) {
-  var indexPage = new Request('index.html')
+  var indexPage = new Request('index.html');
   event.waitUntil(
     fetch(indexPage).then(function(response) {
       return caches.open('offline').then(function(cache) {
-        return cache.put(indexPage, response)
-      })
-  }))
-})
+        return cache.put(indexPage, response);
+      });
+  }));
+});
 self.addEventListener('fetch', function(event) {
   var updateCache = function(request){
     return caches.open('offline').then(function (cache) {
       return fetch(request).then(function (response) {
-        return cache.put(request, response)
-      })
-    })
-  }
-  event.waitUntil(updateCache(event.request))
+        return cache.put(request, response);
+      });
+    });
+  };
+  event.waitUntil(updateCache(event.request));
   event.respondWith(
     fetch(event.request).catch(function(error) {
       return caches.open('offline').then(function (cache) {
         return cache.match(event.request).then(function (matching) {
-          var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching
+          var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
           return report
-        })
-      })
+        });
+      });
     })
-  )
+  );
 })
